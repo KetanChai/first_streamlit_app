51b6489ea8f56aa971b22f1b374b045f3a183b51
Ketan <117353016+KetanChai@users.noreply.github.com>
2022-11-03 16:07:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('CHAI Cafe')
+streamlit.title('🐔CHAI Cafe🥗')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
