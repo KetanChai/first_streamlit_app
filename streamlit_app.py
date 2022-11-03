52a93ffa5848c5607eed346e14cfb4925138bbd3
Ketan <117353016+KetanChai@users.noreply.github.com>
2022-11-03 15:58:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 import streamlit
-
-streamlit.title('CHAI10 Cafe')
+streamlit.title('CHAI Cafe')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥑 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
